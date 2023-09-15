9bc067b3cabf76f0080fd4a50c9c70d5e146ec26
KimJiHong <kjh95044@naver.com>
2023-09-15 16:37:40 +0900

    Feat: cors 추가 #2

--- Project/Back-end/DongDdoNong/src/main/java/com/sit/dongddonong/controller/TestController.java
+++ Project/Back-end/DongDdoNong/src/main/java/com/sit/dongddonong/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.sit.dongddonong.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
     @PostMapping("/test")
     public String test() {
