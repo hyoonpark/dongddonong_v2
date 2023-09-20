8d1c7b16295bc0fda275291b0fada769cc8496a3
zzarru <sn20180301@gmail.com>
2023-09-20 15:47:46 +0900

    Feat: 로그인 성공 후 홈으로 리다이렉트 연결

--- Project/Front-end/dongddonong/src/components/Login/KakaoLogin.js
+++ Project/Front-end/dongddonong/src/components/Login/KakaoLogin.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import KakaoLoginLogo from "../assets/kakao_login_btn.png";
+import KakaoLoginLogo from "../../assets/kakao_login_btn.png";
 
 const KakaoLogin = () => {
   const REST_API_KEY = '4e01b41f45f312031ef4c97ed183fa71'; //REST API KEY
-  // const REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao'; //Redirect URI
-  const REDIRECT_URI = 'https://j9e103.p.ssafy.io/oauth/callback/kakao';
+  const REDIRECT_URI = 'http://localhost:3000/oauth/callback/kakao'; //Redirect URI
+  // const REDIRECT_URI = 'https://j9e103.p.ssafy.io/oauth/callback/kakao';
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const handleLogin = () => {
@@ -18,4 +18,4 @@ const KakaoLogin = () => {
   );
 };
 
-export default KakaoLogin;
+export default KakaoLogin; 
