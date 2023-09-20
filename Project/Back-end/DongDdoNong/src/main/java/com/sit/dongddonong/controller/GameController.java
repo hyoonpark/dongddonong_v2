b649451e54a4defa5f4416257fff2a04535fa74d
KimJiHong <kjh95044@naver.com>
2023-09-20 15:22:12 +0900

    Feat: 경기 목록을 조건(mode, 기간)에 따라 조회 #8

--- Project/Back-end/DongDdoNong/src/main/java/com/sit/dongddonong/controller/GameController.java
+++ Project/Back-end/DongDdoNong/src/main/java/com/sit/dongddonong/controller/GameController.java
@@ -6,13 +6,13 @@ import com.sit.dongddonong.service.GameService;
 import com.sit.dongddonong.service.PlayerHistoryService;
 import com.sit.dongddonong.util.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
 import java.util.List;
 
 @Slf4j
@@ -35,6 +35,7 @@ public class GameController {
     private ApiResponse<String> internalServerErrorHandler(Exception exception) {
         return ApiResponse.fail(500, exception.getMessage());
     }
+
     @Operation(summary = "영상 분석 데이터", description = "영상 분석 데이터를 우리 서버에 넣습니다.")
     @PostMapping
     public ApiResponse<String> createGameAndPlayerHistories(@RequestBody GameDto gameDto) {
@@ -48,24 +49,29 @@ public class GameController {
         return ApiResponse.ok(gameService.getAllGames());
     }
 
-    @Operation(summary = "경기 유저별로 가져오기", description = "모든 경기를 유저별로 가져옵니다.")
-    @GetMapping("/{userId}")
-    public ApiResponse<List<PlayerHistoryDto>> getPlayerHistory(@PathVariable("userId") long userId) {
-        return ApiResponse.ok(playerHistoryService.getPlayerHistory(userId));
-    }
+//    @Operation(summary = "경기 유저별로 가져오기", description = "모든 경기를 유저별로 가져옵니다.")
+//    @GetMapping("/{userId}")
+//    public ApiResponse<List<PlayerHistoryDto>> getPlayerHistory(@PathVariable("userId") long userId) {
+//        return ApiResponse.ok(playerHistoryService.getPlayerHistory(userId));
+//    }
 
     @Operation(summary = "경기 user 연결하기", description = "경기를 유저랑 매핑한다.")
     @PatchMapping("/{PlayerHistoryId}")
-    public ApiResponse<String> putPlayerHistory(@PathVariable("PlayerHistoryId") long PlayerHistoryId, @Parameter long userId) {
-        playerHistoryService.putPlayerHistory(PlayerHistoryId, userId);
+    public ApiResponse<String> putPlayerHistory(@PathVariable("PlayerHistoryId") long playerHistoryId, @RequestParam long userId) {
+        playerHistoryService.putPlayerHistory(playerHistoryId, userId);
         return ApiResponse.ok("성공");
     }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<String> findMatchesByUserId(@@PathVariable String uuid) throws Exception {
-//        UserDto user = userService.login(code);
-//        return ResponseEntity.ok().body(user);
-//
-//    }
+    @Operation(summary = "경기 목록 필터 ", description = "경기를 mode, 기간별로 가져온다. mode가 0이면 mode상관없이 전체")
+    @GetMapping("/{userId}")
+    public ApiResponse<List<PlayerHistoryDto>> getPlayerHistoriesByCondition(
+            @PathVariable("userId") long userId,
+            @RequestParam(required = false, defaultValue = "0") String mode,
+            @RequestParam(required = false) String startDate,
+            @RequestParam(required = false) String endDate)
+            throws ParseException {
+        List<PlayerHistoryDto> result = playerHistoryService.getPlayerHistoriesByCondition(userId, mode, startDate, endDate);
+        return ApiResponse.ok(result);
+    }
 
 }
