70948b30e5a3fc9c14a3bfd557854ab2fc7bc1e9
Bang Jung Woo <qkdqkd7418@naver.com>
2023-09-20 10:57:48 +0900

    Feat: 동영상 업로드 로직 구현 1 #9

--- Project/Front-end/dongddonong/src/App.js
+++ Project/Front-end/dongddonong/src/App.js
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 import Game from "./pages/Game/Game";
 import LoginLoading from "./components/LoginLoading";
 import RecordRoom from "./components/Recordroom/Recordroom"
-
 function App() {
   return (
     <BrowserRouter>
