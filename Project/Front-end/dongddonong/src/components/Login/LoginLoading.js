036b57451854fa51fe8d4bdcc4c3f6059e59e2f2
zzarru <sn20180301@gmail.com>
2023-09-20 17:48:38 +0900

    Feat: token 정보 저장 #5

--- Project/Front-end/dongddonong/src/components/Login/LoginLoading.js
+++ Project/Front-end/dongddonong/src/components/Login/LoginLoading.js
@@ -1,22 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import { userContext } from './../../constexts/userContext';
 
 const LoginLoading = () => {
   const code = new URL(window.location.href).searchParams.get('code');
   // console.log(code);
   const navigator = useNavigate();
+  
+  const { accessToken, setAccessToken } = useState(null);
 
   useEffect(() => {
     axios.get(`https://j9e103.p.ssafy.io:8589/user/login?code=${code}`
+    // axios.get(`http://localhost:8080/user/login?code=${code}`
     ).then((res) => {
       console.log('성공!!', res.data);
-      console.log('엑세스토큰', res.data.accessToken);
-      navigator("/")
+      const token = res.data.data.accessToken
+      setAccessToken(token);
+      console.log(accessToken)
+      navigator('/') // 로그인 시도하던 페이지로 이동
     }).catch(error => {
       console.log('실패!!!!!!', error);
     });
-  }, []);
+  }, [code, setAccessToken]);
 
 
   return (
