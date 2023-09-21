f49632ae5fa01bcaddc535e2fbd12ee96f4eafaa
zzarru <sn20180301@gmail.com>
2023-09-22 05:03:58 +0900

    Feat: 사용자 정보 전역에서 상태관리하기 #5

--- Project/Front-end/dongddonong/src/components/Navbar.js
+++ Project/Front-end/dongddonong/src/components/Navbar.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 
 import KakaoLogin from "./../components/Login/KakaoLogin";
 import LoginModal from "./../components/Login/LoginModal";
+import { useUserContext } from "../constexts/userContext";
+
 import video from "../assets/icon/video.png";
 import VideoModal from "./Modal/VideoModal";
 import Wrapper from "../components/Wrapper";
@@ -28,6 +30,12 @@ const Li = styled.li`
 
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { loggedIn } = useUserContext();
+  
+  const handleLoginClick = () => {
+    setIsModalOpen(true);
+  };
+
   const videoRef = useRef(null);
 
   return (
@@ -43,17 +51,23 @@ const Navbar = () => {
           </Link>
 
           <div className="w-1/4 max-w-xs"></div>
-          <ul className="flex items-center justify-between w-1/3 max-w-xs">
-            <Link to="/game">
-              <Li>경기</Li>
-            </Link>
-
-            <Link to="/recordroom">
-              <Li>기록실</Li>
-            </Link>
-            <Li onClick={() => setIsModalOpen(true)}>로그인</Li>
-          </ul>
-
+          {loggedIn ? (
+            <>
+              <Link to="/game">
+                <Li>경기</Li>
+              </Link>
+              <Link to="/recordroom">
+                <Li>기록실</Li>
+              </Link>
+              <Li>로그아웃</Li>
+            </>
+          ) : (
+            <>
+              <Li onClick={handleLoginClick}>경기</Li>
+              <Li onClick={handleLoginClick}>기록실</Li>
+              <Li onClick={handleLoginClick}>로그인</Li>
+            </>
+          )}
           <button
             onClick={() => {
               videoRef.current.classList.toggle("scale-0");
@@ -61,11 +75,9 @@ const Navbar = () => {
           >
             <img className="w-8" src={video} alt="업로드" />
           </button>
-
           <VideoModal ref={videoRef}></VideoModal>
         </div>
       </Wrapper>
-
       <LoginModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
