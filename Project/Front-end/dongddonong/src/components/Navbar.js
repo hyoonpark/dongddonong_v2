59cb04fe3be77f838de92cc30d912fc59b617bff
이동주 <leednj0113@naver.com>
2023-09-21 17:41:38 +0900

    Feat: 상단 스크롤 #10

--- Project/Front-end/dongddonong/src/components/Navbar.js
+++ Project/Front-end/dongddonong/src/components/Navbar.js
@@ -5,7 +5,8 @@ import styled from "styled-components";
 import KakaoLogin from "./../components/Login/KakaoLogin";
 import LoginModal from "./../components/Login/LoginModal";
 import video from "../assets/icon/video.png";
-import VideoModal from "./VideoModal";
+import VideoModal from "./Modal/VideoModal";
+import Wrapper from "../components/Wrapper";
 
 const Li = styled.li`
   position: relative;
@@ -31,37 +32,39 @@ const Navbar = () => {
 
   return (
     <nav className="h-20 border-b border-black">
-      <div className="relative flex items-center justify-between h-20 px-4 mx-auto max-w-7xl">
-        <Link to="/">
-          <img
-            className="h-14"
-            src={`${process.env.PUBLIC_URL}/logo.png`}
-            alt="로고"
-          />
-        </Link>
-
-        <div className="w-1/4 max-w-xs"></div>
-        <ul className="flex items-center justify-between w-1/3 max-w-xs">
-          <Link to="/game">
-            <Li>경기</Li>
+      <Wrapper>
+        <div className="relative flex items-center justify-between h-20">
+          <Link to="/">
+            <img
+              className="h-14"
+              src={`${process.env.PUBLIC_URL}/logo.png`}
+              alt="로고"
+            />
           </Link>
 
-          <Link to="/recordroom">
-            <Li>기록실</Li>
-          </Link>
-          <Li onClick={() => setIsModalOpen(true)}>로그인</Li>
-        </ul>
+          <div className="w-1/4 max-w-xs"></div>
+          <ul className="flex items-center justify-between w-1/3 max-w-xs">
+            <Link to="/game">
+              <Li>경기</Li>
+            </Link>
 
-        <button
-          onClick={() => {
-            videoRef.current.classList.toggle("scale-0");
-          }}
-        >
-          <img className="w-8" src={video} alt="업로드" />
-        </button>
+            <Link to="/recordroom">
+              <Li>기록실</Li>
+            </Link>
+            <Li onClick={() => setIsModalOpen(true)}>로그인</Li>
+          </ul>
 
-        <VideoModal ref={videoRef}></VideoModal>
-      </div>
+          <button
+            onClick={() => {
+              videoRef.current.classList.toggle("scale-0");
+            }}
+          >
+            <img className="w-8" src={video} alt="업로드" />
+          </button>
+
+          <VideoModal ref={videoRef}></VideoModal>
+        </div>
+      </Wrapper>
 
       <LoginModal
         isOpen={isModalOpen}
