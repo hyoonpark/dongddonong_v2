7d4afc166875a205bef4274a4baaa68317e08ffb
hayoonpark <the.day0619@gmail.com>
2023-09-25 11:08:19 +0900

    Test

--- Project/AI/DongDdoNong/app.py
+++ Project/AI/DongDdoNong/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_file
-from video import upload
+# from video import upload
 
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def index():
     return "Hello World!"
 
 
-@app.route('/ai/upload', methods=['POST'])
-def upload_file():
-    rslt = upload.module()
-    return rslt
+# @app.route('/ai/upload', methods=['POST'])
+# def upload_file():
+#     rslt = upload.module()
+#     return rslt
