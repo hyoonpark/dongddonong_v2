c55c3057536adb763180045927aef294fd488353
hayoonpark <the.day0619@gmail.com>
2023-09-25 10:48:20 +0900

    Feat: update setting file

--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -2,7 +2,7 @@ from app import app
 import ssl
 
 if __name__ == "__main__":
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     ssl_context.load_cert_chain('certificate.crt', 'private.key')
     
-    app.run(port=443, ssl_context=ssl_context)
+    app.run(host="0.0.0.0", port=443, ssl_context=ssl_context)
