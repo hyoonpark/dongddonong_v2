08071bb88c7d2dfadf7b3fbbc0e5b7187afe38ed
hayoonpark <the.day0619@gmail.com>
2023-09-26 14:48:50 +0900

    Test

--- Project/AI/DongDdoNong/app.py
+++ Project/AI/DongDdoNong/app.py
@@ -21,12 +21,12 @@ s3 = boto3.client(
 )
 
 
-@app.before_request
-def before_request():
-    if request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
+# @app.before_request
+# def before_request():
+#     if request.url.startswith('http://'):
+#         url = request.url.replace('http://', 'https://', 1)
+#         code = 301
+#         return redirect(url, code=code)
 
 
 @app.route('/')
--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -3,5 +3,5 @@ import ssl
 
 if __name__ == "__main__":
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    ssl_context.load_cert_chain(certfile='./crt/certificate.pem', keyfile='./crt/private.pem')
-    app.run(port=5000, ssl_context=ssl_context)
+    ssl_context.load_cert_chain(certfile='./crt/certificate.pem', keyfile='./crt/private.pem',password='1234')
+    app.run(port=443, ssl_context=ssl_context)
