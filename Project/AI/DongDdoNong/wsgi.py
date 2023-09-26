158d6fead1e734216b7da7ef6ea2457fe0f1ac2c
hayoonpark <the.day0619@gmail.com>
2023-09-26 11:14:39 +0900

    Test

--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     ssl_context.load_cert_chain('./crt/certificate.crt', './crt/private.key')
     
-    app.run()
+    app.run(ssl_context=ssl_context, debug=True)
