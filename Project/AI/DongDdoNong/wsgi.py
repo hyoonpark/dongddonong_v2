f2f40a737fdd417866ebc600ad9173eb3231b8b5
김지홍 <kjh95044@naver.com>
2023-09-26 15:32:48 +0900

    Ts

--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     # ssl_context.load_cert_chain(certfile='./crt/certificate.pem', keyfile='./crt/private.pem',password='1234')
     # app.run(host="0.0.0.0", port=5000, ssl_context=ssl_context)
-    app.run(host="0.0.0.0", port=5000, ssl_context=('./crt/certificate.pem', './crt/private.pem'))
+    app.run()
