8c09e3606792b955c3f88dbe1870449fca5ee157
hayoonpark <the.day0619@gmail.com>
2023-09-26 11:51:02 +0900

    Test

--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -1,8 +1,10 @@
 from app import app
-# import ssl
+from flask_sslify import SSLify
+
+sslify = SSLify(app)
 
 if __name__ == "__main__":
     # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    # ssl_context.load_cert_chain('./crt/certificate.pem', './crt/private.pem')
+    ssl_context = ('./crt/certificate.pem', './crt/private.pem')
     
-    app.run(ssl_context='adhoc')
+    app.run(ssl_context=ssl_context)
