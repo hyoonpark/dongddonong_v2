4d174c4f62564b06400ff91205802bbb49d10abc
hayoonpark <the.day0619@gmail.com>
2023-09-26 11:25:58 +0900

    Test

--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -2,7 +2,7 @@ from app import app
 import ssl
 
 if __name__ == "__main__":
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    ssl_context.load_cert_chain('./crt/certificate.crt', './crt/private.key')
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    ssl_context.load_cert_chain('./crt/certificate.pem', './crt/private.pem')
     
     app.run(ssl_context=ssl_context, debug=True)
