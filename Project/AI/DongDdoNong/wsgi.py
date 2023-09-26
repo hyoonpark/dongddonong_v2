fb806f16cd19762827a0463e3e9c2f995ffb0dcc
hayoonpark <the.day0619@gmail.com>
2023-09-26 14:21:34 +0900

    Feat:update wsgi.py

--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -1,6 +1,7 @@
 from app import app
-
+import ssl
 
 if __name__ == "__main__":
-
-    app.run()
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='./crt/certificate.pem', keyfile='./crt/private.pem', password='secret')
+    app.run(port=443, ssl_context=ssl_context)
