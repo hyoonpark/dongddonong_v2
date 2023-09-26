f518c988b98527d39e13a71c6b39423034e59a1c
hayoonpark <the.day0619@gmail.com>
2023-09-26 12:12:28 +0900

    Feat: ssl

--- Project/AI/DongDdoNong/app.py
+++ Project/AI/DongDdoNong/app.py
@@ -21,15 +21,6 @@ s3 = boto3.client(
 )
 
 
-@app.before_request
-def before_request():
-    scheme = request.headers.get('X-Forwarded-Proto')
-    if scheme and scheme == 'http' and request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
-
-
 @app.route('/')
 def index():
     return "Hello World!"
--- Project/AI/DongDdoNong/wsgi.py
+++ Project/AI/DongDdoNong/wsgi.py
@@ -1,9 +1,9 @@
 from app import app
+from flask_sslify import SSLify
 
 
-
+sslify = SSLify(app)
 
 if __name__ == "__main__":
 
-    
     app.run()
