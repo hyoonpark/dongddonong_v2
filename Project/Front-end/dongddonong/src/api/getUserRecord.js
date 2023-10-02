dca090e9a6fa76540dc3c5a6e92e0bf3305acf47
zzarru <sn20180301@gmail.com>
2023-10-02 17:06:24 +0900

    Test: redirectURI 수정

--- Project/Front-end/dongddonong/src/api/getUserRecord.js
+++ Project/Front-end/dongddonong/src/api/getUserRecord.js
@@ -1,7 +1,6 @@
 import axiosInstance from "./axiosConfig";
 
 // 유저가 참여한 경기 데이터 가져오기
-
 export const getUserRecord = async (userId) => {
   try {
     const res = await axiosInstance.get(`/game/assign/${userId}`);
