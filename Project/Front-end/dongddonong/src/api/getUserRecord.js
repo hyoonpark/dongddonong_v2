1f5f67c859bb808036081dec2287cbed12e52c81
zzarru <sn20180301@gmail.com>
2023-10-02 16:20:12 +0900

    Feat: axios 요청 #12

--- Project/Front-end/dongddonong/src/api/getUserRecord.js
+++ Project/Front-end/dongddonong/src/api/getUserRecord.js
@@ -1,16 +1,14 @@
-// import axiosInstance from "./axiosConfig";
-// import useUserContext from "../contexts/userContext"
+import axiosInstance from "./axiosConfig";
 
-// // 유저가 참여한 경기 데이터 가져오기
-// const {user} = useUserContext()
-// const userId = user.id
-// const getUserRecord = async () => {
-//   try {
-//     const res = await axiosInstance.get(`/game/assign/${userId}`);
-//     console.log(res.data)
-//     return res.data
-//   } catch (err) {
-//     return err
-//   }
-// };
-// export default getUserRecord
+// 유저가 참여한 경기 데이터 가져오기
+
+const getUserRecord = async (userId) => {
+  try {
+    const res = await axiosInstance.get(`/game/assign/${userId}`);
+    console.log('요청성공!', res.data)
+    return res.data
+  } catch (err) {
+    return err
+  }
+};
+export default getUserRecord
