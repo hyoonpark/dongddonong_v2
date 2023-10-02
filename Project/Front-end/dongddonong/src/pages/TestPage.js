2868dba3f14468d2ff4e5f6decb2e42e15769a48
zzarru <sn20180301@gmail.com>
2023-10-02 16:40:01 +0900

    Test: getRecord 요청

--- Project/Front-end/dongddonong/src/pages/TestPage.js
+++ Project/Front-end/dongddonong/src/pages/TestPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 // import { getGameAll } from '../api/gameApi';
-import getUserRecord from '../api/getUserRecord';
+import { getUserRecord } from '../api/getUserRecord';
 import { useUserContext } from "../contexts/userContext"
 
 
