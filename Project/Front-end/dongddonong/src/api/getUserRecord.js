10629aee2362bf4709e2f87169af5be6f945ff76
jungwoo <qkdqkd7418@naver.com>
2023-10-02 16:04:12 +0900

    Feat: 기록실 자료 axios 요청에 토큰 부여 #7

--- Project/Front-end/dongddonong/src/api/getUserRecord.js
+++ Project/Front-end/dongddonong/src/api/getUserRecord.js
@@ -1,16 +1,13 @@
-// import axiosInstance from "./axiosConfig";
-// import useUserContext from "../contexts/userContext"
+import axiosInstance from "./axiosConfig";
 
-// // 유저가 참여한 경기 데이터 가져오기
-// const {user} = useUserContext()
-// const userId = user.id
-// const getUserRecord = async () => {
-//   try {
-//     const res = await axiosInstance.get(`/game/assign/${userId}`);
-//     console.log(res.data)
-//     return res.data
-//   } catch (err) {
-//     return err
-//   }
-// };
-// export default getUserRecord
+// 유저가 참여한 경기 데이터 가져오기
+export const getUserRecord = async (userId) => {
+  try {
+    console.log(userId);
+    const res = await axiosInstance.get(`/game/assign/${userId}`);
+    console.log(res.data)
+    return res.data.data
+  } catch (err) {
+    return err
+  }
+};
