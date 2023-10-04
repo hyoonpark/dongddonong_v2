7bd81d663932fc423380e1b6df834bc79509b48a
hayoonpark <the.day0619@gmail.com>
2023-10-04 18:52:51 +0900

    test

--- Project/AI/DongDdoNong/deepsort/basketball.py
+++ Project/AI/DongDdoNong/deepsort/basketball.py
@@ -6,8 +6,8 @@ sys.path.append(NOW_DIR + '/models')
 sys.path.append(NOW_DIR + '/pose')
 sys.path.append(NOW_DIR + '/deep_sort_pytorch')
 
-from utils.datasets import LoadStreams, LoadImages
 from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_imshow, non_max_suppression, scale_coords, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, time_synchronized, TracedModel
