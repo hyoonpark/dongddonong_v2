937e78ecb2501f13089112b26171224783979b74
hayoonpark <the.day0619@gmail.com>
2023-10-04 14:36:54 +0900

    Feat: video analyze

--- Project/AI/DongDdoNong/app.py
+++ Project/AI/DongDdoNong/app.py
@@ -10,8 +10,11 @@ from scenedetect.detectors import ContentDetector
 from scenedetect.scene_manager import save_images
 from scenedetect.video_splitter import split_video_ffmpeg
 from moviepy.editor import VideoFileClip, concatenate_videoclips, vfx, AudioFileClip, afx
+from deepsort import basketball
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip, AudioFileClip
 
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 app = Flask(__name__)
 
 
@@ -35,49 +38,46 @@ def analyze_video(ID):
     try:
         # Lambda 함수로부터 전달된 동영상 데이터 받기
         video_data = request.data
+        result = basketball.detect(save_img=False, video_data, ID)
 
         # 동영상 데이터를 메모리에서 읽기
-        nparr = np.fromstring(video_data, np.uint8)
-        video_frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-        # 동영상 분석 로직 구현
-        # 여기에서 동영상 프레임을 처리하고 분석 작업
-        # 예를 들어, 프레임별로 얼굴 인식, 객체 추적
-        clf = joblib.load("저장된 모델.pkl 경로")  # 모델 로드
-        prediction = clf.predict(video_frame)  # 예측값
+        # nparr = np.fromstring(video_data, np.uint8)
+        # video_frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         # 분석 결과 반환
-        analysis_result = prediction  # ex) 카카오톡 ID 1/1, 50, 60
-        highlight = highlight_video(analysis_result, video_data)
-        return jsonify({'result': analysis_result}), 200
+        # highlight = highlight_video(result, video_data)
+
+
+        return jsonify({'result': result}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
 def highlight_video(result, video_data):
-    # 디텍터 생성, 임계값 30, 장면 당 최소 프레임 수 150
-    content_detector = ContentDetector(threshold=30, min_scene_len=150)
+    for _ in range(len(result["playerHistories"])):
+        # 각 슛 장면 주변 2초씩 가져오기
+        highlight_duration = 2  # 2초
+        for frame in result["playerHistories"].get("goalTime"):
+            start_time = max(frame - highlight_duration, 0)
+            end_time = frame + highlight_duration
+            output_file = f'highlight_{frame}.mp4'
 
-    # Scene Manager 생성
-    scene_manager = SceneManager()
-    scene_manager.add_detector(content_detector)
+            # 동영상의 해당 부분 추출
+            ffmpeg_extract_subclip(video_data, start_time, end_time, targetname=output_file)
 
-    # 디텍트 수행
-    scene_manager.detect_scenes(video_data, show_progress=True)
-    scene_list = scene_manager.get_scene_list()
+            # 추출된 하이라이트 동영상에 배경 음악 추가
+            highlight_clip = VideoFileClip(output_file)
+            bgm_clip = AudioFileClip(bgm_path)
 
-    # 장면 분할 결과 출력
-    for scene in scene_list:
-        start, end = scene
-        print(start, "~", end)
+            # 무음 오디오 클립 생성
+            silence_clip = AudioFileClip("", fps=44100, duration=highlight_clip.duration)
 
-    # 영상 자르기 (파일로 저장)
-    split_video_ffmpeg(video_path, scene_list, show_progress=True)
+            # 하이라이트 동영상에 배경 음악을 무음 오디오 클립과 함께 합치기
+            final_clip = highlight_clip.set_audio(silence_clip).set_audio(bgm_clip)
 
-    clip1 = VideoFileClip("test-Scene-001.mp4").subclip(0, 20).fx(vfx.speedx, 0.5).fx(vfx.fadein, 1)
-    clip2 = VideoFileClip("test-Scene-002.mp4").subclip(0, 8)
-    clip3 = VideoFileClip("test-Scene-003.mp4").subclip(1, 6)
-    clip4 = VideoFileClip("test-Scene-004.mp4").subclip(1, 6).fx(vfx.fadeout, 1)
+            # 하이라이트 동영상 저장
+            final_clip.write_videofile(output_file)
 
-    combined = concatenate_videoclips([clip1, clip2, clip3, clip4])
-    combined.write_videofile("combined.mp4")
+        clips = [VideoFileClip(f'highlight_{frame}.mp4') for frame in shot_frames]
+        final_video = concatenate_videoclips(clips)
+        final_video.write_videofile('highlight_summary.mp4')
