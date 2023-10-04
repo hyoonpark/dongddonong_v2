c033564c89e12ac718b3020f75525a84c65b9ae2
KimJiHong <kjh95044@naver.com>
2023-10-04 16:05:42 +0900

    Feat: 미할당 경기, 분석중인 경기 gameId 가져오는 api 추가 14#

---
+++ Project/Back-end/DongDdoNong/src/main/java/com/sit/dongddonong/dto/game/GameIdDto.java
@@ -0,0 +1,14 @@
+package com.sit.dongddonong.dto.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameIdDto {
+    private Long gameId;
+}
