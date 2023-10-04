8e86c96271ee8793d9400f10d57b37077f505968
jungwoo <qkdqkd7418@naver.com>
2023-10-05 00:59:54 +0900

    Feat: getUserProfile api를 작성해 유저의 이름과 프로필 사진을 사용 #7

---
+++ Project/Front-end/dongddonong/src/api/getUserProfile.js
@@ -0,0 +1,14 @@
+import axiosInstance from "./axiosConfig";
+
+// 유저가 참여한 경기 데이터 가져오기
+export const getUserProfile = async (id) => {
+  try {
+    const res = await axiosInstance.get(`/user/info`, {
+        params: { userId: id },
+      } )
+    console.log('요청성공!', res.data)
+    return res.data.data
+  } catch (err) {
+    return err
+  }
+};
--- Project/Front-end/dongddonong/src/components/Recordroom/RecordCard.js
+++ Project/Front-end/dongddonong/src/components/Recordroom/RecordCard.js
@@ -2,20 +2,48 @@ import user2 from "../../assets/player2.jpg";
 import rightArrow from "../../assets/icon/right-arrow.png";
 import leftArrow from "../../assets/icon/left-arrow.png";
 import { useUserContext } from "../../contexts/userContext";
+import { getUserProfile } from "../../api/getUserProfile"
+import { useState, useEffect } from "react";
+
+
 
 const RecordCard = (props) => {
   const players = [];
   const { user } = useUserContext();
-  let player1 = null;
-  let player2 = null;
+  
+  let player1 = null
+  let player2 = null
+  
+  const [player1Profile, setPlayer1Profile] = useState({})
+  const [player2Profile, setPlayer2Profile] = useState({})
+
+
   for (const key in props.game) {
     players.push(key);
   }
-  if (players.length > 2) {
-    player1 = players[0];
-    player2 = players[1];
+  if (players.length > 2) { // 투바나 대전이다
+    if (players[0] === user.id) { // 좌측에 내가 담겼으면 해서 조건문을 단다.
+      player1 = players[0];
+      player2 = players[1];
+    }
+    else {
+      player2 = players[0]
+      player1 = players[1]
+    }
+
+    getUserProfile(player1).then((res) => { // 닉네임과 이미지주소를
+      setPlayer1Profile({'nickName': res.nickName, 'src': res.profileImgUrl})
+    })
+
+    getUserProfile(player2).then((res) => { 
+      setPlayer2Profile({'nickName': res.nickName, 'src': res.profileImgUrl})
+    })
+
   } else {
     player1 = players[0];
+    getUserProfile(player1).then((res) => { 
+      setPlayer1Profile({'nickName': res.nickName, 'src': res.profileImgUrl})
+    })
   }
 
   return (
@@ -25,11 +53,11 @@ const RecordCard = (props) => {
         <div className="h-28 flex gap-1">
           <div className="flex flex-col items-center justify-evenly ml-2 w-1/5">
             <img
-              src={user.profileImgUrl}
+              src={player1Profile.src}
               className="rounded-2xl w-14 h-16"
               alt="어웨이"
             />
-            <div className="text-[12px]">{player1}</div>
+            <div className="text-[12px]">{player1Profile.nickName}</div>
           </div>
 
           <div className="flex items-center justify-center w-3/5 relative">
@@ -68,8 +96,8 @@ const RecordCard = (props) => {
           </div>
           {player2 && (
             <div className="flex flex-col items-center justify-evenly mr-2 w-1/5">
-              <img src={user2} className="rounded-2xl w-14 h-16" alt="어웨이" />
-              <div className="text-[12px]">{player2}</div>
+              <img src={player2Profile.src} className="rounded-2xl w-14 h-16" alt="어웨이" />
+              <div className="text-[12px]">{player2Profile.nickName}</div>
             </div>
           )}
         </div>
