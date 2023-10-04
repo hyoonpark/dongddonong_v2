cda0d6ecffc6da0fbeb67ccc7b12b0542f1bb260
hayoonpark <the.day0619@gmail.com>
2023-10-04 15:54:41 +0900

    test

--- Project/AI/DongDdoNong/app.py
+++ Project/AI/DongDdoNong/app.py
@@ -9,8 +9,8 @@ import boto3
 # from scenedetect.video_splitter import split_video_ffmpeg
 from moviepy.editor import VideoFileClip, concatenate_videoclips, vfx, AudioFileClip, afx
 from deepsort import basketball
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-from moviepy.editor import VideoFileClip, AudioFileClip
+# from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+# from moviepy.editor import VideoFileClip, AudioFileClip
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -52,31 +52,31 @@ def analyze_video(ID):
         return jsonify({'error': str(e)}), 500
 
 
-def highlight_video(result, video_data):
-    for _ in range(len(result["playerHistories"])):
-        # 각 슛 장면 주변 2초씩 가져오기
-        highlight_duration = 2  # 2초
-        for frame in result["playerHistories"].get("goalTime"):
-            start_time = max(frame - highlight_duration, 0)
-            end_time = frame + highlight_duration
-            output_file = f'highlight_{frame}.mp4'
-
-            # 동영상의 해당 부분 추출
-            ffmpeg_extract_subclip(video_data, start_time, end_time, targetname=output_file)
-
-            # 추출된 하이라이트 동영상에 배경 음악 추가
-            highlight_clip = VideoFileClip(output_file)
-            bgm_clip = AudioFileClip(bgm_path)
-
-            # 무음 오디오 클립 생성
-            silence_clip = AudioFileClip("", fps=44100, duration=highlight_clip.duration)
-
-            # 하이라이트 동영상에 배경 음악을 무음 오디오 클립과 함께 합치기
-            final_clip = highlight_clip.set_audio(silence_clip).set_audio(bgm_clip)
-
-            # 하이라이트 동영상 저장
-            final_clip.write_videofile(output_file)
-
-        clips = [VideoFileClip(f'highlight_{frame}.mp4') for frame in shot_frames]
-        final_video = concatenate_videoclips(clips)
-        final_video.write_videofile('highlight_summary.mp4')
+# def highlight_video(result, video_data):
+#     for _ in range(len(result["playerHistories"])):
+#         # 각 슛 장면 주변 2초씩 가져오기
+#         highlight_duration = 2  # 2초
+#         for frame in result["playerHistories"].get("goalTime"):
+#             start_time = max(frame - highlight_duration, 0)
+#             end_time = frame + highlight_duration
+#             output_file = f'highlight_{frame}.mp4'
+#
+#             # 동영상의 해당 부분 추출
+#             ffmpeg_extract_subclip(video_data, start_time, end_time, targetname=output_file)
+#
+#             # 추출된 하이라이트 동영상에 배경 음악 추가
+#             highlight_clip = VideoFileClip(output_file)
+#             bgm_clip = AudioFileClip(bgm_path)
+#
+#             # 무음 오디오 클립 생성
+#             silence_clip = AudioFileClip("", fps=44100, duration=highlight_clip.duration)
+#
+#             # 하이라이트 동영상에 배경 음악을 무음 오디오 클립과 함께 합치기
+#             final_clip = highlight_clip.set_audio(silence_clip).set_audio(bgm_clip)
+#
+#             # 하이라이트 동영상 저장
+#             final_clip.write_videofile(output_file)
+#
+#         clips = [VideoFileClip(f'highlight_{frame}.mp4') for frame in shot_frames]
+#         final_video = concatenate_videoclips(clips)
+#         final_video.write_videofile('highlight_summary.mp4')
