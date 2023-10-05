cc6f683d4f26c2451bf061e8cea47a01765fc85b
이동주 <leednj0113@naver.com>
2023-10-06 06:29:05 +0900

    Update HighLight.js

--- Project/Front-end/dongddonong/src/components/Game/HighLight.js
+++ Project/Front-end/dongddonong/src/components/Game/HighLight.js
@@ -36,10 +36,12 @@ const HighLight = (props) => {
       />
       <ReactPlayer
         url="https://dongddonong.s3.ap-northeast-2.amazonaws.com/1266.mp4"
-        type="mp4"
         controls
         ref={videoRef}
         width={"100%"}
+        poster={
+          "https://mblogthumb-phinf.pstatic.net/MjAxNzA1MzBfMTY4/MDAxNDk2MTQ3NTcyNDU3.o-Ua56y_QljYHt-b_67A2ychiUtDtPFJsDTCr0E_7a8g.XiGG5dAW__FwxmLlDKH7iYArlVx01vSQneQtFlXtMPsg.JPEG.dashlady1/%EC%99%BC%EC%86%90%EC%9D%80%EA%B1%B0%EB%93%A4%EB%BF%90.jpg?type=w800"
+        }
       ></ReactPlayer>
 
       <Toast toastOpen={toastOpen}>링크를 클립보드에 복사했습니다.</Toast>
