e7755f4ddd41b5c23a5161f5d2dc21994f023cd2
Bang Jung Woo <qkdqkd7418@naver.com>
2023-10-05 11:29:11 +0900

    Fix: axios 무한호출 수정 #7

--- Project/Front-end/dongddonong/src/components/Recordroom/RecordCard.js
+++ Project/Front-end/dongddonong/src/components/Recordroom/RecordCard.js
@@ -4,54 +4,70 @@ import leftArrow from "../../assets/icon/left-arrow.png";
 import { useUserContext } from "../../contexts/userContext";
 import { getUserProfile } from "../../api/getUserProfile"
 import { useState, useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
 
 
 
 const RecordCard = (props) => {
   const players = [];
   const { user } = useUserContext();
-  
-  let player1 = null
-  let player2 = null
-  
+
   const [player1Profile, setPlayer1Profile] = useState({})
   const [player2Profile, setPlayer2Profile] = useState({})
+
+
+  let isPractice = null
 
 
   for (const key in props.game) {
     players.push(key);
   }
-  if (players.length > 2) { // 투바나 대전이다
-    if (players[0] === user.id) { // 좌측에 내가 담겼으면 해서 조건문을 단다.
-      player1 = players[0];
-      player2 = players[1];
+  useEffect(() => {
+    console.log(players.length, '조건문이 돌기전 마지막 불꽃')
+    if (players.length > 2) {
+      if (players[0] === user.id) {
+        getUserProfile(players[0]).then((res) => { // 닉네임과 이미지주소를
+          setPlayer1Profile({ 'nickName': res.nickName, 'total': props.game[players[0]]["total"], 'src': res.profileImgUrl })
+          console.log(player1Profile)
+        })
+
+        getUserProfile(players[1]).then((res) => {
+          setPlayer2Profile({ 'nickName': res.nickName, 'total': props.game[players[1]]["total"], 'src': res.profileImgUrl })
+        })
+      }
+      else {
+        getUserProfile(players[0]).then((res) => { // 닉네임과 이미지주소를
+          setPlayer2Profile({ 'nickName': res.nickName, 'total': props.game[players[0]]["total"], 'src': res.profileImgUrl })
+          console.log(player1Profile)
+        })
+
+        getUserProfile(players[1]).then((res) => {
+          setPlayer1Profile({ 'nickName': res.nickName, 'total': props.game[players[1]]["total"], 'src': res.profileImgUrl })
+        })
+      }
     }
     else {
-      player2 = players[0]
-      player1 = players[1]
+      getUserProfile(players[0]).then((res) => {
+        setPlayer1Profile({ 'nickName': res.nickName, 'total': props.game[players[0]]["total"], 'src': res.profileImgUrl })
+      })
+      console.log('1인')
+      console.log(props)
     }
+  }, [props])
 
-    getUserProfile(player1).then((res) => { // 닉네임과 이미지주소를
-      setPlayer1Profile({'nickName': res.nickName, 'src': res.profileImgUrl})
-    })
 
-    getUserProfile(player2).then((res) => { 
-      setPlayer2Profile({'nickName': res.nickName, 'src': res.profileImgUrl})
-    })
 
-  } else {
-    player1 = players[0];
-    getUserProfile(player1).then((res) => { 
-      setPlayer1Profile({'nickName': res.nickName, 'src': res.profileImgUrl})
-    })
+  if (players.length > 2) { // 투바나 대전이다
+    isPractice = 1
   }
+
 
   return (
     <div className="flex flex-col mt-4 gap-3">
       <div className="h-32 border-black rounded border">
         <div className="h-4"></div>
         <div className="h-28 flex gap-1">
-          <div className="flex flex-col items-center justify-evenly ml-2 w-1/5">
+          <div className="flex flex-col items-center justify-evenly mr-2 ml-2 w-1/5">
             <img
               src={player1Profile.src}
               className="rounded-2xl w-14 h-16"
@@ -67,10 +83,10 @@ const RecordCard = (props) => {
 
             <div className="h-1/2 flex items-center relative w-full">
               <div className="absolute left-0 text-xl">
-                {props.game[player1]["total"]}
+                {player1Profile.total}
               </div>
-              {player2 &&
-                props.game[player2]["total"] < props.game[player1]["total"] && (
+              {isPractice &&
+                player2Profile.total < player1Profile.total && (
                   <div className="absolute left-10">
                     <img
                       className="w-3 -translate-y-px"
@@ -79,8 +95,8 @@ const RecordCard = (props) => {
                     />
                   </div>
                 )}
-              {player2 &&
-                props.game[player2]["total"] > props.game[player1]["total"] && (
+              {isPractice &&
+                player2Profile.total > player1Profile.total && (
                   <div className="absolute right-10">
                     <img
                       className="w-3 -translate-y-px"
@@ -90,12 +106,12 @@ const RecordCard = (props) => {
                   </div>
                 )}
               <div className="absolute right-0 text-xl">
-                {player2 && props.game[player2]["total"]}
+                {isPractice && player2Profile.total}
               </div>
             </div>
           </div>
-          {player2 && (
-            <div className="flex flex-col items-center justify-evenly mr-2 w-1/5">
+          {isPractice && (
+            <div className="flex flex-col items-center justify-evenly ml-2 mr-2 w-1/5">
               <img src={player2Profile.src} className="rounded-2xl w-14 h-16" alt="어웨이" />
               <div className="text-[12px]">{player2Profile.nickName}</div>
             </div>
